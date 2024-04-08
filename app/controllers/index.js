b3933e7679b35f7ce2f3b72adf6727a77620359c
HarineKannan <harinesakthe@gmail.com>
2024-04-08 11:34:54 +0530

    second commit

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -33,17 +33,90 @@ export default class IndexController extends Controller {
     await this.fetchData('http://localhost:8080/LogFetcher/logFetcher', payload);
   }
 
- 
 
   generatePayload() {
     return {
-      neededLog: this.selectedLogType,
-      fieldsNeeded: [
-        this.showTimestamp && 'timegenerated',
-        this.showEventCode && 'eventcode',
-        this.showSourcename && 'sourcename',
-        this.showMessage && 'message'
-      ].filter(Boolean)
+      logtype: this.selectedLogType
     };
   }
 }
+
+
+
+// import Controller from '@ember/controller';
+// import { action } from '@ember/object';
+
+// export default class IndexController extends Controller {
+
+//   logsData = [];
+
+//   @action
+//   selectLogType(logType) {
+//     console.log("Selected Log Type:", logType);
+//   }
+
+//   async fetchData(url, payload) {
+//     try {
+//       const response = await fetch(url, {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(payload)
+//       });
+
+//       if (response.ok) {
+//         console.log('Operation successful');
+//       } else {
+//         throw new Error('Operation failed');
+//       }
+//     } catch (error) {
+//       console.error('Error:', error.message);
+//     }
+//   }
+
+//   // async getData(url, payload) {
+//   //   try {
+//   //     const response = await fetch(url, {
+//   //       method: 'GET',
+//   //       headers: {
+//   //         'Content-Type': 'application/json'
+//   //       },
+//   //       body: JSON.stringify(payload)
+//   //     });
+
+//   //     if (response.ok) {
+//   //       const data = await response.json();
+//   //       this.logsData = data;
+//   //       console.log('Operation successful');
+//   //     } else {
+//   //       throw new Error('Operation failed');
+//   //     }
+//   //   } catch (error) {
+//   //     console.error('Error:', error.message);
+//   //   }
+//   // }
+
+//   @action
+//   async fetchLogs() {
+//     const payload = this.generatePayload();
+//     await this.fetchData('http://localhost:8080/LogFetcher/logFetcher', payload);
+//   }
+
+
+//   generatePayload() {
+//     return {
+//       logtype: this.selectedLogType
+//     };
+//   }
+  
+//   // @action
+//   // async searchLogs(query) {
+//   //   const payload = {
+//   //     query: query
+//   //   };
+//   //   await this.getData('http://localhost:8080/LogFetcher/logFetcher', payload);
+//   // }
+
+
+// }
